43121a3e19367c6b5d890abae62a4f867282acd0
jangg0627 <jangg0627@naver.com>
2022-11-20 09:50:02 +0900

    python challenge day20-21

--- python, 100day challenge/day20~21, snake_game/scoreboard.py
+++ python, 100day challenge/day20~21, snake_game/scoreboard.py
@@ -4,6 +4,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", "r") as file:
+            self.high_score = int(file.read())
         self.hideturtle()
         self.penup()
         self.color("white")
@@ -11,16 +13,24 @@ class ScoreBoard(Turtle):
         self.write_score()
 
     def write_score(self):
-        self.write(f"Score: {self.score}", align="center", font=('Arial', 20, 'normal'))
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align="center", font=('Arial', 20, 'normal'))
 
     def eat_food(self):
-        self.clear()
         self.score += 1
         self.write_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=('Arial', 20, 'normal'))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(str(self.score))
+        self.score = 0
+        self.write_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align="center", font=('Arial', 20, 'normal'))
 
 
 
